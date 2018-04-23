5870ca7b4dca17079e8642575b6531df5fb207f2
ioanasandu <36401830+ioanasandu@users.noreply.github.com>
2018-04-24 00:07:55 +0300

    Ajustarea clasei Transaction,astfel ca Expense si Earning sa o extinda. Modificarea BD cu tabelul Transactions in care apar Expenses si Earnings. + corectare cf mesaj (metoda update fara posibilitate de modificare a userId).

--- src/main/java/ro/happydevs/intellifin/services/TransactionService.java
+++ src/main/java/ro/happydevs/intellifin/services/TransactionService.java
@@ -1,21 +1,30 @@
 package ro.happydevs.intellifin.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ro.happydevs.intellifin.models.Transaction;
+import ro.happydevs.intellifin.models.User;
+import ro.happydevs.intellifin.repositories.TokenRepository;
 import ro.happydevs.intellifin.repositories.TransactionRepository;
 import ro.happydevs.intellifin.repositories.UserRepository;
 
+import java.util.ArrayList;
+
 public class TransactionService {
 
+    private static Logger logger = LoggerFactory.getLogger(AccountService.class);
     private TransactionRepository transactionRepository = new TransactionRepository();
+    private TokenRepository tokenRepository = new TokenRepository();
 
-    public void createTransaction(Transaction transaction, String token){
+    public boolean createTransaction(Transaction transaction, String token) {
+
         UserService userService = new UserService();
 
+        logger.info("[Transaction Service Create] - Called");
+
         transaction.setUserId(userService.getUserForToken(token).getId());
-
-        transactionRepository.create(transaction);
-
-
+        return transactionRepository.create(transaction);
 
     }
+
 }

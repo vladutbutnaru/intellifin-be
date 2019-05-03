44a57321b08308d88b1acb7ff81149d0126a0e48
Vlad <vlad2me@gmail.com>
2019-05-03 16:48:47 +0300

    Init of new env

---
+++ src/main/java/ro/happydevs/intellifin/utils/security/DBConfig.java
@@ -0,0 +1,16 @@
+package ro.happydevs.intellifin.utils.security;
+
+//@Configuration
+public class DBConfig {
+
+//    @Bean
+//    public DataSource dataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//            dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://104.248.39.93:3306/intellifin?useSSL=false&autoReconnect=true&useUnicode=true");
+//        dataSource.setUsername( "remote" );
+//        dataSource.setPassword( "MysqlPassword1!" );
+//        return dataSource;
+//    }
+
+}
